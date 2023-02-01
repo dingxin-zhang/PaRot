4019278d1f8530d181ed815658a6422272a32bfe
PatchRot <aaai2023parot@outlook.com>
2023-02-01 21:25:46 +0800

    init

--- models/model_util.py
+++ models/model_util.py
@@ -1,17 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-@Author: Yue Wang
-@Contact: yuewangx@mit.edu
-@File: model.py
-@Time: 2018/10/13 6:35 PM
-
-Modified by
-@Author: An Tao
-@Contact: ta19@mails.tsinghua.edu.cn
-@Time: 2020/3/9 9:32 PM
-"""
-
 import os
 import sys
 import copy
@@ -161,59 +147,6 @@ def get_graph_feature_three_dir(center_xyz, feat, k=20):
     feature = torch.cat([grouped_feat, normed_group_feat, R_ji, dot_pos, dot_pos2, rel_dist], dim=-1)
 
     return feature.permute(0, 3, 1, 2).contiguous()
-
-#
-#
-# def get_graph_feature_three_dir_edge_nosub(center_xyz, feat, k=20):
-#     # x: (B, N, 3), feat: (B, N, C)
-#     center_xyz = center_xyz.contiguous()
-#     batch_size, num_points = center_xyz.size()[:2]
-#
-#     knn_idx = knn_point(k, center_xyz, center_xyz)  # (B, N, K)
-#     grouped_xyz = index_points(center_xyz, knn_idx)  # (B, N, K, 3)
-#     grouped_feat = index_points(feat, knn_idx)  # (B, N, K, C)
-#
-#     dir_xy = feat[..., -6:].view(batch_size, num_points, 1, 2, 3)  # (B, N, 1, 2, 3)
-#     dir_z = dir_xy[:, :, :, 0].cross(dir_xy[:, :, :, 1])  # (B, N, 1, 3)
-#     dir_z = F.normalize(dir_z, dim=-1).unsqueeze(-2)  # (B, N, 1, 1, 3)
-#     dir_xyz = torch.cat((dir_xy, dir_z), dim=-2).repeat(1, 1, k, 1, 1)  # (B, N, K, 3, 3)
-#
-#     grouped_dir_xy = grouped_feat[..., -6:].view(batch_size, num_points, k, 2, 3)  # (B, N, K, 2, 3)
-#     grouped_dir_z = grouped_dir_xy[:, :, :, 0].cross(grouped_dir_xy[:, :, :, 1])  # (B, N, K, 3)
-#     grouped_dir_z = F.normalize(grouped_dir_z, dim=-1).unsqueeze(-2)  # (B, N, K, 1, 3)
-#     grouped_dir_xyz = torch.cat((grouped_dir_xy, grouped_dir_z), dim=-2)  # (B, N, K, 3, 3)
-#
-#     R_ji = (grouped_dir_xyz @ dir_xyz.transpose(-1, -2)).view(batch_size, num_points, k, 9)
-#
-#     # normed grouped feature
-#     grouped_feat = grouped_feat[..., :128]  # (B, N, K, 128)
-#     feat = feat[..., :128]  # (B, N, 128)
-#     #normed_group_feat = grouped_feat - feat.unsqueeze(-2)  # (B, N, K, 128)
-#     normed_group_feat = feat.unsqueeze(-2).repeat(1, 1, k, 1)  # (B, N, K, 128)
-#
-#     # relative point distance
-#     rel_xyz = grouped_xyz - center_xyz.unsqueeze(-2)  # (B, N, K, 3)
-#     rel_dist = torch.norm(rel_xyz, dim=-1, keepdim=True)  # (B, N, K, 1)
-#     rel_xyz = F.normalize(rel_xyz, dim=-1).unsqueeze(-1)  # (B, N, K, 3, 1)
-#     dot_pos = (dir_xyz @ rel_xyz).squeeze(-1)  # (B, N, K, 3)
-#     dot_pos2 = (grouped_dir_xyz @ rel_xyz).squeeze(-1)  # (B, N, K, 3)
-#     feature = torch.cat([grouped_feat, normed_group_feat, R_ji, dot_pos, dot_pos2, rel_dist], dim=-1)
-#
-#     return feature.permute(0, 3, 1, 2).contiguous()
-
-# def get_orientation(dir_vectors):
-#     Ori_x = dir_vectors[:, 0]  # (BN, 3)
-#     Ori_y = dir_vectors[:, 1]  # (BN, 3)
-#     Ori_z = F.normalize(torch.cross(Ori_x, Ori_y), dim=-1)
-#     Ori_xyz = torch.stack([Ori_x, Ori_y, Ori_z], dim=-2)  # (BN, 3, 3)
-#     return Ori_xyz
-#
-# def get_orientation_detach(dir_vectors):
-#     Ori_x = dir_vectors[:, 0]  # (BN, 3)
-#     Ori_y = dir_vectors[:, 1]  # (BN, 3)
-#     Ori_z = F.normalize(torch.cross(Ori_x, Ori_y), dim=-1).detach()
-#     Ori_xyz = torch.stack([Ori_x, Ori_y, Ori_z], dim=-2)  # (BN, 3, 3)
-#     return Ori_xyz
 
 def get_orientation(dir_vectors):
     Ori_x = dir_vectors[:, 0]  # (BN, 3)
